cb8e941e1fe680778d15e7b2691fa57233ad92a5
ntim_cx <kwaku315@gmail.com>
2020-11-03 12:35:06 +0000

    firebase initialization

---
+++ js/firebase/firebase-init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDxpCAacgpbPa-iG9BBSCTXdDeiyj2LV60",
+    authDomain: "quizlet-web-eng.firebaseapp.com",
+    databaseURL: "https://quizlet-web-eng.firebaseio.com",
+    projectId: "quizlet-web-eng",
+    storageBucket: "quizlet-web-eng.appspot.com",
+    messagingSenderId: "5642343960",
+    appId: "1:5642343960:web:f586ba4c094d0442cb156d",
+    measurementId: "G-RPMF9FNZWF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
